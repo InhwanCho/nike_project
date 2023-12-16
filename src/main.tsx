533db029b1cd87086785c3fa2afc3592b3392ec3
InhwanCho <whdlsghks2@gmail.com>
2023-12-16 13:27:05 +0900

    fixing env error

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,11 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
+const PUBLIC_URL = import.meta.env.PUBLIC_URL;
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
