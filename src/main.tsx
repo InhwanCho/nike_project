3f1fe1cd5a2c25c3b7c7a91a3d9e5b28901ca431
InhwanCho <whdlsghks2@gmail.com>
2023-12-16 13:39:24 +0900

    fixing env error

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter basename={import.meta.env.VITE_PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
