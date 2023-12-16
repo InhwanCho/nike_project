3d7b386480dd6eb7f83d4aefd18718bbbe1b577d
InhwanCho <whdlsghks2@gmail.com>
2023-12-16 13:43:36 +0900

    fixing env error

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.VITE_PUBLIC_URL}>
+    <BrowserRouter basename='https://nike-project-omega.vercel.app'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
